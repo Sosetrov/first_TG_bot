51b13f98fb4c8c7fb64c527b9198742d261181cc
Sosetrov <Osetrov_stepan@mail.ru>
2022-10-02 12:18:09 +0500

    Add the project to repository

---
+++ src/main/java/BotLogic/BotLogic.java
@@ -0,0 +1,45 @@
+package BotLogic;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class BotLogic extends TelegramLongPollingBot {
+        private final String BotUsername;
+        private final String BotToken;
+        //  private final
+        public BotLogic(){
+         this.BotUsername = BotUsername;
+         this.BotToken = BotToken;
+        }
+
+        @Override
+        public String getBotUsername() {
+            return BotUsername;
+        }
+
+        @Override
+        public String getBotToken() {
+            return BotToken;
+        }
+
+        @Override
+        public void onUpdateReceived(Update update) {
+            @Override
+            public void onUpdateReceived(Update update) {
+                // We check if the update has a message and the message has text
+                if (update.hasMessage() && update.getMessage().hasText()) {
+                    SendMessage message = new SendMessage(); // Create a SendMessage object with mandatory fields
+                    message.setChatId(update.getMessage().getChatId().toString());
+                    message.setText(update.getMessage().getText());
+
+                    try {
+                        execute(message); // Call method to send the message
+                    } catch (TelegramApiException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
--- src/main/java/org/stepanandmilana/App.java
+++ src/main/java/org/stepanandmilana/App.java
@@ -1,13 +1,26 @@
 package org.stepanandmilana;
 
+import BotLogic.BotLogic;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
 /**
  * Hello world!
  *
  */
-public class App 
+public class App
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static  void main(String[] args){
+
+        String TELEGRAM_BOT_USERNAME = System.getenv("TELEGRAM_BOT_USERNAME");
+        String TELEGRAM_BOT_TOKEN = System.getenv("TELEGRAM_BOT_TOKEN");
+
+        try {
+            TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+            botsApi.registerBot(new BotLogic());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
     }
 }
